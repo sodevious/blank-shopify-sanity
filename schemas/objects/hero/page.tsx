22b137b23c94eb73cab8e00c8bf51c9636dc2a0a
Nicole Dominguez <sodevious.net@gmail.com>
2022-06-26 13:16:25 -0400

    init

---
+++ schemas/objects/hero/page.tsx
@@ -0,0 +1,33 @@
+export default {
+  name: 'hero.page',
+  title: 'Page hero',
+  type: 'object',
+  fields: [
+    // Title
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'text',
+      rows: 3,
+    },
+    // Content
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      validation: (Rule) => Rule.max(1),
+      of: [
+        {
+          name: 'productWithVariant',
+          title: 'Product with variant',
+          type: 'productWithVariant',
+        },
+        {
+          name: 'imageWithProductHotspots',
+          title: 'Image',
+          type: 'imageWithProductHotspots',
+        },
+      ],
+    },
+  ],
+}
